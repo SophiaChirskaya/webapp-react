5f58825fc813f7a2d7f6845f94d70f00f569ba09
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-11 14:31:50 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+    return (
+        <nav>
+        <div>
+            <Link to="/">Home Page</Link>
+        </div>
+        </nav>
+    )
+}
+
+export default Header
