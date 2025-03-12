a1f971c7e58435580a3073b83917078af205e41f
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-12 18:25:34 +0100

    [add]MovieCard to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,23 @@
 import axios from 'axios'
+import MovieCard from '../components/MovieCard'
 
 import { useState, useEffect } from "react"
 
 const HomePage = () => {
     return (
         <>
-        <h1>Home Page</h1>
+        <h1 className='text-primary'>Bool Movies</h1>
+        <h2><i>The greatest movies ever</i></h2>
+
+        <div className='row row-cols-3 mt-4'>
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+
+        </div>
         </>
     )
 }
