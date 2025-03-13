a33dc9fffa86359eb7b6514d9a3662e9a89e7034
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-03-13 09:23:20 +0100

    [feat]HomePage dinamic fetures

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,19 +4,46 @@ import MovieCard from '../components/MovieCard'
 import { useState, useEffect } from "react"
 
 const HomePage = () => {
+
+    // settiamo lo stato del componente
+    const [movies, setMovies] = useState([]);
+    // funzione fetching
+    const fetchMovies = () => {
+        axios.get("http://localhost:3000/api/movies")
+        .then(
+            res => {
+                setMovies(res.data)
+            }
+        )
+        .catch(err => console.log(err)
+        )
+    }
+
+    // fetch movies
+    useEffect(fetchMovies, [])
+
+    // funzione rendering delle MovieCard
+    const renderMovies = () => {
+        return movies.map(
+            movie => {
+                return (
+                    <div className='col' key={movie.id} >
+                        <MovieCard movieProp={movie} />
+
+                    </div>
+                )
+            }
+        )
+    }
+
+
     return (
         <>
         <h1 className='text-primary'>Bool Movies</h1>
         <h2><i>The greatest movies ever</i></h2>
 
         <div className='row row-cols-3 mt-4'>
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-            <MovieCard />
-
+            {renderMovies()}
         </div>
         </>
     )
